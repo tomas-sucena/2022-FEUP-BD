e8e9b230980d3be0c970cb112bfcf6631c77b72c
tomas-sucena <tomas21sucena@gmail.com>
2022-11-13 16:00:05 +0000

    its all coming along

--- criar.sql
+++ criar.sql
@@ -1,10 +1,63 @@
 CREATE TABLE Época(
-    ano INT NOT NULL PRIMARY KEY
+    ano                     INTEGER PRIMARY KEY
 );
 
 CREATE TABLE Equipa(
-    idEquipa INT NOT NULL PRIMARY KEY AUTOINCREMENT,
-    nome TEXT NOT NULL,
-    género TEXT NOT NULL,
-    idClube INT NOT NULL REFERENCES Clube(idClube)
+    idEquipa                INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome                    TEXT NOT NULL,
+    género                  BOOLEAN,
+    idClube                 INTEGER NOT NULL REFERENCES Clube(idClube)
+);
+
+CREATE TABLE ÉpocaEquipa(
+    idÉpoca                 INTEGER,
+    idEquipa                INTEGER,
+    
+    PRIMARY KEY (idÉpoca, idEquipa),
+    FOREIGN KEY (idÉpoca) REFERENCES Época(idÉpoca),
+    FOREIGN KEY (idEquipa) REFERENCES Equipa(idEquipa)
+);
+
+CREATE TABLE Jogador(
+    idJogador               INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome                    TEXT NOT NULL,
+    dataNascimento          DATE,
+    género                  BOOLEAN,
+    altura                  DECIMAL(1, 2) CONSTRAINT alturaPositiva CHECK (altura > 0),
+    peso                    INTEGER CONSTRAINT pesoPositivo CHECK (peso > 0),
+    nacionalidade           TEXT NOT NULL,
+    numCamisola             INTEGER CONSTRAINT numVálido CHECK (numCamisola >= 0 and numCamisola < 100),
+    idEquipa                INTEGER NOT NULL REFERENCES Equipa(idEquipa)
+);
+
+CREATE TABLE Clube(
+    idClube                 INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome                    TEXT NOT NULL,
+    dataFundação            DATE,
+    idAssociação            INTEGER NOT NULL REFERENCES Associação(idAssociação)
+);
+
+CREATE TABLE Associação(
+    idAssociação            INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome                    TEXT NOT NULL,
+    distrito                VARCHAR(17) NOT NULL,                
+    cidade                  TEXT NOT NULL,
+    dataFundação            DATE NOT NULL  
+);
+
+CREATE TABLE Estádio(
+    idEstádio               INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome                    TEXT NOT NULL,
+    morada                  TEXT NOT NULL,
+    lotação                 INTEGER CONSTRAINT lotaçãoPositiva CHECK (lotação > 0)
+);
+
+CREATE TABLE Jogo(
+    idJogo                  INTEGER PRIMARY KEY AUTOINCREMENT,
+    fase                    TEXT NOT NULL,
+    data                    DATE NOT NULL,
+    jornada                 TEXT NOT NULL,
+    idEstádio               INTEGER NOT NULL REFERENCES Estádio(idEstádio),
+    idEquipaVisitante       INTEGER NOT NULL REFERENCES Equipa(idEquipa),
+    idEquipaVisitada        INTEGER NOT NULL REFERENCES Equipa(idEquipa)
 );
