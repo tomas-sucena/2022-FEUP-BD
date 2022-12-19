2a66bee8d358c41f4939d7c1152b8b6de6031323
Sucena <tomas21sucena@gmail.com>
2022-12-19 17:44:46 +0000

    created int5

--- src/queries/int5.sql
+++ src/queries/int5.sql
@@ -2,5 +2,14 @@
 .headers on
 .nullvalue NULL
 
-/* Qual o terceiro número mais comum usado para identificar um jogador dentro de uma equipa? Liste o número como NÚMERO 
-e a quantidade de vezes que este foi utilizado como TOTAL." 
+/* Quais os 10 números de camisola de jogador mais comuns? Liste os números como NÚMERO 
+e a quantidade de vezes que estes foram utilizados como TOTAL. Em caso de empate, ordene
+os números por ordem crescente. */
+
+SELECT ej.numCamisola AS 'NÚMERO', count(ej.numCamisola) AS 'TOTAL'  
+FROM EquipaJogador ej
+WHERE ej.numCamisola IS NOT NULL
+GROUP BY 1
+HAVING max(2)
+ORDER BY 2 DESC, 1
+LIMIT 10
--- src/queries/int6.sql
+++ src/queries/int6.sql
@@ -1,3 +1,10 @@
 .mode columns
 .headers on
 .nullvalue NULL
+
+/* Qual o pior marcador com altura entre 1.85-1.95 m, que não tenha nacionalidade americana e que jogue como extremo? 
+Selecione o nome do jogador como JOGADOR, a altura como ALTURA, o país de origem como PAÍS, a posição a que joga como 
+POSIÇÃO e o número de pontos que marcou como PONTOS. */
+
+SELECT j.nome as NOME, j.altura as ALTURA, j.pais as PAÍS 
+FROM Jogador j
