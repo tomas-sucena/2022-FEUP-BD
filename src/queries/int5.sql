990fb9e51c40705d4d370ad3b8997ff5b862919a
Bruno Drumond <93215985+ItsDrumond@users.noreply.github.com>
2022-12-19 17:07:21 +0000

    int5

--- src/queries/int5.sql
+++ src/queries/int5.sql
@@ -1,3 +1,6 @@
 .mode columns
 .headers on
 .nullvalue NULL
+
+/* Qual o terceiro número mais comum usado para identificar um jogador dentro de uma equipa? Liste o número como NÚMERO 
+e a quantidade de vezes que este foi utilizado como TOTAL." 
