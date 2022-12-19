170309f5a067d02ced34611b3362db59aac9c0f9
Bruno Drumond <93215985+ItsDrumond@users.noreply.github.com>
2022-12-19 16:47:42 +0000

    fix

--- src/queries/int4.sql
+++ src/queries/int4.sql
@@ -1,10 +1,16 @@
+.mode columns
+.headers on
+.nullvalue NULL
+
+/* Quais as equipas que, em todos os jogos da época, nunca conseguiram marcar mais de 70 pontos? Liste a equipa por ID e NOME.*/
+
 with t1 as
 (select e.id, count(*) as NR_JOGOS
 from equipa e, jogo j
 where j.idEquipaCasa = e.id or j.idEquipaFora = e.id
 )
 
-select e.id, e.nome
+select e.id as "ID", e.nome as "NOME"
 from equipa e, jogo j, t1 
 where (j.idEquipaCasa = e.id and j.pontosEquipaCasa <= 70) or (j.idEquipaFora = e.id and j.pontosEquipaFora <= 70) and t1.id = e.id
 having count(*) = t1.NR_JOGOS; 
