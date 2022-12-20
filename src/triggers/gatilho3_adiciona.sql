bae3fe5b457caae6b55a30a677e39ec9636c0e18
Bruno Drumond <93215985+ItsDrumond@users.noreply.github.com>
2022-12-20 21:45:38 +0000

    drop kick no pc

--- src/triggers/gatilho3_adiciona.sql
+++ src/triggers/gatilho3_adiciona.sql
@@ -5,7 +5,7 @@
 PRAGMA FOREIGN_KEYS = ON;
 
 -- pequena descricao
-CREATE TABLE avisoJogador(
+CREATE TABLE log(
     id                  INTEGER PRIMARY KEY AUTOINCREMENT,
     idJogador           INTEGER NOT NULL REFERENCES Jogador(idJogador),
     mensagem            TEXT NOT NULL
@@ -15,8 +15,8 @@ CREATE TRIGGER verificarJogador_altura_peso
 AFTER INSERT ON Jogador
 WHEN NEW.altura IS NULL AND NEW.peso IS NULL
 BEGIN
-    INSERT INTO avisoJogador (idJogador, mensagem)
-    VALUES (NEW.idJogador, 'O jogador ' || NEW.nome || ' não apresenta um valor para a altura nem para o peso!');
+    INSERT INTO log (idJogador, mensagem)
+    VALUES (NEW.idJogador, "O jogador " || NEW.nome || " não apresenta um valor para a altura nem para o peso!");
 END;
 
 
@@ -24,14 +24,14 @@ CREATE TRIGGER verificarJogador_altura
 AFTER INSERT ON Jogador
 WHEN NEW.altura IS NULL AND NEW.peso IS NOT NULL
 BEGIN
-    INSERT INTO avisoJogador (idJogador, mensagem)
-    VALUES (NEW.idJogador, 'O jogador ' || NEW.nome || ' não apresenta um valor para a altura!');
+    INSERT INTO log (idJogador, mensagem)
+    VALUES (NEW.idJogador, "O jogador " || NEW.nome || " não apresenta um valor para a altura!");
 END;
         
 CREATE TRIGGER verificarJogador_peso
 AFTER INSERT ON Jogador
 WHEN NEW.peso IS NULL AND NEW.altura IS NOT NULL
 BEGIN
-    INSERT INTO avisoJogador (idJogador, mensagem)
-    VALUES (NEW.idJogador, 'O jogador ' || NEW.nome || ' não apresenta um valor para o peso!');
+    INSERT INTO log (idJogador, mensagem)
+    VALUES (NEW.idJogador, "O jogador " || NEW.nome || " não apresenta um valor para o peso!");
 END;
--- src/triggers/gatilho3_remove.sql
+++ src/triggers/gatilho3_remove.sql
@@ -4,7 +4,7 @@
 
 PRAGMA FOREIGN_KEYS = ON;
 
-DROP TABLE IF EXISTS avisoJogador;
+DROP TABLE IF EXISTS log;
+DROP TRIGGER IF EXISTS verificarJogador_altura_peso;
 DROP TRIGGER IF EXISTS verificarJogador_altura;
 DROP TRIGGER IF EXISTS verificarJogador_peso;
-DROP TRIGGER IF EXISTS verificarJogador_altura_peso;
