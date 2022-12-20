099aa3fc5f22f274d752b1ab5bc9f6fc628ad6ce
Sucena <tomas21sucena@gmail.com>
2022-12-20 12:09:58 +0000

    trigger2 finally works

--- src/triggers/gatilho2_adiciona.sql
+++ src/triggers/gatilho2_adiciona.sql
@@ -10,7 +10,7 @@ FOR EACH ROW
 WHEN EXISTS
     (SELECT *
     FROM Jogo j
-    WHERE j.idRecinto = NEW.idRecinto AND j.dataJogo = NEW.dataJogo AND abs(j.horaInicio - NEW.horaInicio) < '2:00')
+    WHERE j.idRecinto = NEW.idRecinto AND j.dataJogo = NEW.dataJogo AND abs(j.horaInicio - NEW.horaInicio) < 2)
 BEGIN
     SELECT RAISE(ABORT, 'O jogo que está a tentar inserir coincide com outro jogo que ocorre no mesmo recinto');
 END;
