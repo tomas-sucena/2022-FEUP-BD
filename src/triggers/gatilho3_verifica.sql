e9267432c588c2597d8e6e76a7f117f4fa042fe7
Bruno Drumond <93215985+ItsDrumond@users.noreply.github.com>
2022-12-20 18:21:35 +0000

    ave

--- src/triggers/gatilho3_verifica.sql
+++ src/triggers/gatilho3_verifica.sql
@@ -5,8 +5,15 @@
 INSERT INTO Jogador 
 VALUES (272930, 'Aaron Broussard', 'Aaron Broussard', 1990-04-14, 'Estados Unidos da América', 'M', 196, NULL);
 
-INSERT INTO Jogador 
-VALUES (279035, 'Andrew KosteckaIII', 'Andrew Edward KosteckaIII ', 1997-08-19, 'Estados Unidos da América', 'M', NULL, NULL);
+.print ''
+.print 'Something Something'
+.print ''
+
+SELECT * FROM avisoJogador;
+
+.print ''
+.print 'Something Something'
+.print ''
 
 INSERT INTO Jogador 
 VALUES (176602, 'Diogo Peixe', 'Diogo Janeiro Peixe', 2000-01-17, 'Portugal', 'M', NULL, 89);
@@ -16,3 +23,20 @@ VALUES (176602, 'Diogo Peixe', 'Diogo Janeiro Peixe', 2000-01-17, 'Portugal', 'M
 .print ''
 
 SELECT * FROM avisoJogador;
+
+.print ''
+.print 'Something Something'
+.print ''
+
+INSERT INTO Jogador 
+VALUES (279035, 'Andrew KosteckaIII', 'Andrew Edward KosteckaIII ', 1997-08-19, 'Estados Unidos da América', 'M', NULL, NULL);
+
+.print ''
+.print 'Something Something'
+.print ''
+
+SELECT * FROM avisoJogador;
+
+.print ''
+.print 'Something Something'
+.print ''
