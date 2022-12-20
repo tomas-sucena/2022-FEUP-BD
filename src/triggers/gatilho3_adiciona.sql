29bc8ce4dba601d3ad2a037cc622b1361d9de0b7
Bruno Drumond <93215985+ItsDrumond@users.noreply.github.com>
2022-12-20 17:36:18 +0000

    gatilho 3 test

---
+++ src/triggers/gatilho3_adiciona.sql
@@ -0,0 +1,25 @@
+.mode columns
+.headers on
+.nullvalue NULL
+
+DROP TABLE IF EXISTS infoJogador;
+
+CREATE TABLE infoJogador(
+    id                  INTEGER AUTO_INCREMENT,
+    idJogador           INTEGER,
+    mensagem            TEXT NOT NULL,
+    PRIMARY KEY (id, idJogador)
+);
+
+CREATE TRIGGER verificarJogador
+AFTER INSERT ON Jogador
+FOR EACH ROW
+BEGIN
+    IF NEW.altura IS NULL THEN
+        INSERT INTO infoJogador (idJogador, mensagem)
+        VALUES(new.id,CONCAT('O jogador', NEW.nome, ', não apresenta um valor para a altura!'))
+    IF NEW.peso IS NULL THEN
+        INSERT INTO infoJogador (idJogador, mensagem)
+        VALUES(new.id,CONCAT('O jogador', NEW.name, ', não apresenta um valor para o peso!'))
+        END IF;
+END;
