b00185b3001aa89001fb86091106c3d15a0e253e
shamiko <up202108672@up.pt>
2022-12-20 16:38:05 +0000

    No

--- src/queries/int4.sql
+++ src/queries/int4.sql
@@ -11,8 +11,8 @@ where j.idEquipaCasa = e.idEquipa or j.idEquipaFora = e.idEquipa
 group by 1
 )
 
-select e.id as "ID", e.nome as "NOME"
+select e.idEquipa as "ID", e.nome as "NOME"
 from equipa e, jogo j, t1 
-where (j.idEquipaCasa = e.idEquipa and j.pontosEquipaCasa <= 70) or (j.idEquipaFora = e.idEquipa and j.pontosEquipaFora <= 70) and t1.idEquipa = e.idEquipa
+where (j.idEquipaCasa = e.idEquipa and j.pontosEquipaCasa <= 100) or (j.idEquipaFora = e.idEquipa and j.pontosEquipaFora <= 100) and t1.idEquipa = e.idEquipa
 group by 1,2
 having count(j.idJogo) = t1.NR_JOGOS; 
