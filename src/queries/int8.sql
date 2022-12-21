1a6a18a736cf701848fa22c7559e190e93bb8eab
shamiko <up202108672@up.pt>
2022-12-21 15:55:25 +0000

    added query 8

--- src/queries/int8.sql
+++ src/queries/int8.sql
@@ -3,3 +3,8 @@
 .nullvalue NULL
 
 /*Liste o nome e os pontos marcados do jogador que marcou mais pontos num jogo na época de 2021/22, em caso de empate liste o que tem menor número de camisola(se for possivel)*/
+
+
+SELECT j.idJogador as 'idJogador', j.nome as 'NOME',l.idJogo as 'JOGO', sum(l.pontos) as 'PONTOS'
+from Jogador j INNER JOIN Lance l on j.idJogador = l.idJogador
+GROUP BY 1,2,3
