60f8a05ca26aed77683a85ce628b027c93121176
shamiko <up202108672@up.pt>
2022-12-21 13:40:18 +0000

    added query 8

--- src/queries/int8.sql
+++ src/queries/int8.sql
@@ -1,3 +1,5 @@
 .mode columns
 .headers on
 .nullvalue NULL
+
+/*Liste o nome e os pontos marcados do jogador que marcou mais pontos num jogo na época de 2021/22, em caso de empate liste o que tem menor número de camisola(se for possivel)*/
