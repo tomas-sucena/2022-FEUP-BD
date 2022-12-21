68f5b239d36ceee0c0a82694d85173a327acf1e1
shamiko <up202108672@up.pt>
2022-12-21 16:06:31 +0000

    completed query 8

--- src/queries/int8.sql
+++ src/queries/int8.sql
@@ -5,8 +5,14 @@
 
 /*Liste o nome e os pontos marcados do jogador que marcou mais pontos num jogo na época de 2021/22, em caso de empate liste o que tem menor número de camisola(se for possivel)*/
 
-
-SELECT j.idJogador as 'idJogador', j.nome as 'NOME',l.idJogo as 'JOGO', sum(l.pontos) as 'PONTOS'
+with pontos as
+(SELECT j.idJogador as 'idJogador', j.nome as 'NOME',l.idJogo as 'JOGO', sum(l.pontos) as 'PONTOS'
 from Jogador j INNER JOIN Lance l on j.idJogador = l.idJogador
-GROUP BY 1,2,3
+GROUP BY 1,2)
 
+SELECT p.nome, p.pontos
+from pontos p INNER JOIN EquipaJogador ej on p.idJogador = ej.idJogador
+group by 1
+having max(p.pontos)
+order by ej.numCamisola
+limit 1
