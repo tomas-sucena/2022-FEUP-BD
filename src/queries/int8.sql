17b2c3bb4704d608d79ff6974a5deb32d216572f
Sucena <tomas21sucena@gmail.com>
2022-12-21 16:07:39 +0000

    da vinki?

--- src/queries/int8.sql
+++ src/queries/int8.sql
@@ -2,4 +2,7 @@
 .headers on
 .nullvalue NULL
 
-/* Liste o nome e os pontos marcados do jogador que marcou mais pontos num jogo na época de 2021/22, em caso de empate liste o que tem menor número de camisola(se for possivel)*/
+/* Para um dado jogador, o field goal percentage, cuja abreviatura é FG%, é o rácio entre o número de lançamentos que
+ resultaram em pontos e o número total de lançamentos efetuados.
+ Com base nessa informação, quais foram as 3 equipas cujos jogadores, na fase regular, apresentaram a melhor FG%?
+ Liste o nome das equipas como 'EQUIPA' e a média da FG% como 'FG%'. */
